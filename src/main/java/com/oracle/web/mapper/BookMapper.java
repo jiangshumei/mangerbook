9a2a13aa8e0b5651397c5a2870819654240ba540
jiangshumei <jiangshumei@qq.com>
2019-05-27 15:07:30 +0800

    项目搭建

---
+++ src/main/java/com/oracle/web/mapper/BookMapper.java
@@ -0,0 +1,22 @@
+package com.oracle.web.mapper;
+
+import com.oralce.web.bean.Book;
+import com.oralce.web.bean.SubBook;
+
+import java.util.List;
+
+public interface BookMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Book record);
+
+    Book selectByPrimaryKey(Integer id);
+
+    List<Book> selectAll();
+
+    int updateByPrimaryKey(Book record);
+
+	List<SubBook> selectAllWithOneSQL();
+
+	List<SubBook> showByPageHelper();
+}
